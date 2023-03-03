c56a7fdeeec71f7c6d8453d73e3d840e513a7047
Chris Suszyński <ksuszyns@redhat.com>
2023-03-03 16:29:50 +0100

    Workaround for https://github.com/quarkusio/quarkus/issues/31587 and https://github.com/cloudevents/sdk-java/issues/533

--- quarkus/src/main/java/com/redhat/openshift/knative/showcase/events/Endpoint.java
+++ quarkus/src/main/java/com/redhat/openshift/knative/showcase/events/Endpoint.java
@@ -1,13 +1,7 @@
 package com.redhat.openshift.knative.showcase.events;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cloudevents.CloudEvent;
-import io.cloudevents.core.builder.CloudEventBuilder;
-import io.cloudevents.http.restful.ws.StructuredEncoding;
 import io.cloudevents.jackson.JsonFormat;
-import io.quarkus.runtime.StartupEvent;
 import io.smallrye.mutiny.Multi;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.jboss.resteasy.reactive.RestStreamElementType;
@@ -15,18 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.random.RandomGenerator;
 
 @Path("events")
 @ApplicationScoped
@@ -35,64 +24,32 @@ class Endpoint {
   private static final Logger LOGGER = LoggerFactory.getLogger(Endpoint.class);
   private final List<CloudEvent> events = new ArrayList<>();
 
-  void init(@Observes StartupEvent ignored, ObjectMapper om)
-    throws JsonProcessingException {
-    var rg = RandomGenerator.getDefault();
-    for (int i = 0; i < 3; i++) {
-      var s = Score.random(rg);
-      events.add(CloudEventBuilder.v1()
-        .withId(String.valueOf(i))
-        .withSource(URI.create("//localhost/dev"))
-        .withType(Endpoint.class.getName())
-        .withData(MediaType.APPLICATION_JSON, om.writeValueAsBytes(s))
-        .build());
-    }
-  }
-
   @GET
   @Operation(summary = "Retrieves all registered events as a JSON stream")
-  @RestStreamElementType(MediaType.APPLICATION_JSON)
-  @StructuredEncoding(JsonFormat.CONTENT_TYPE)
-  public Multi<CloudEvent> events() {
-    return Multi.createFrom().iterable(events);
-  }
-
-  @GET
-  @Path("last")
-  @Produces(MediaType.APPLICATION_JSON)
-  @StructuredEncoding(JsonFormat.CONTENT_TYPE)
-  public CloudEvent last() {
-    return events.get(events.size() - 1);
+  @RestStreamElementType(JsonFormat.CONTENT_TYPE)
+  public Multi<Event> events() {
+    return Multi.createFrom()
+      .iterable(events)
+      .map(this::workaroundQuarkus31587);
   }
 
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
+  @Consumes({MediaType.APPLICATION_JSON, JsonFormat.CONTENT_TYPE})
   @Operation(summary = "Receives a CloudEvent and stores it")
   public void receive(CloudEvent event) {
     events.add(event);
-    LOGGER.info("Received event: {}", event);
+    LOGGER.debug("Received event: {}", event);
   }
 
-  private static final class Score {
-    @JsonProperty
-    Play play;
-    @JsonProperty
-    int score;
-
-    static Score random(RandomGenerator rg) {
-      var s = new Score();
-      s.score = rg.nextInt(1_000);
-      s.play = new Play();
-      s.play.id = new UUID(rg.nextLong(), rg.nextLong()).toString();
-      s.play.game = rg.nextInt(300);
-      return s;
-    }
+  /**
+   * A workaround for
+   * <a href="https://github.com/quarkusio/quarkus/issues/31587">quarkusio/quarkus#31587</a>
+   * and <a href="https://github.com/cloudevents/sdk-java/issues/533">cloudevents/sdk-java#533</a>.
+   *
+   * TODO: Remove this method once the above issues is fixed.
+   */
+  private Event workaroundQuarkus31587(CloudEvent event) {
+    return Event.from(event, om);
   }
-
-  private static class Play {
-    @JsonProperty
-    String id;
-    @JsonProperty
-    Integer game;
   }
 }
